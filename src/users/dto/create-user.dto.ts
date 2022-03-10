2717b0f24c602fe716e35cecdd61123da3729258
paullee714 <paul_lee@kakao.com>
2022-03-11 00:27:37 +0900

    feat: add REST API CRUD Resource

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  nickname: string;
+  email: string;
+  password: string;
+  bio: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    console.log('createUserDto: ', createUserDto);
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
